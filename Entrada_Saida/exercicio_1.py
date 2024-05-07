a8d246c7abd6d87ffdbe726c3a47f5c4803a07e9
NetoFrazao <joaofrazao054@gmail.com>
2024-05-07 10:07:54 -0300

    python1

--- Entrada_Saida/exercicio_1.py
+++ Entrada_Saida/exercicio_1.py
@@ -1,4 +1,6 @@
-nota1 = input("Digite a primeira nota: ")
-nota2 = input("Digite a segunda nota: ")
-media = nota1+nota2
-print = media
+numero1 = int(input("Insira o primeiro número inteiro: "))
+numero2 = int(input("Insira o segundo número inteiro: "))
+print("Você inseriu os números:", numero1, "e", numero2)
+soma = (numero1 + numero2)
+produto = (numero1 + numero2)
+print(f"A soma dos números: { soma }" f" O produto dos números: { produto}")
