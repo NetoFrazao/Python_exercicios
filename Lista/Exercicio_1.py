fd0a45b9b94053396039780e3742191b4f7f36a5
NetoFrazao <joaofrazao054@gmail.com>
2024-05-23 11:00:12 -0300

    Update Exercicio_1.py

---
+++ Lista/Exercicio_1.py
@@ -0,0 +1,7 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+dobro = [numero * 2 for numero in numeros]
+
+
+
+print(f"Lista com numeros sem o dobro: {numeros}")
+print(f"Lista com numeros com o dobro: {dobro}")
