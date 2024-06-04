20eb4ecbd67b3c7c495557fdbf1a5f81eb2be793
NetoFrazao <joaofrazao054@gmail.com>
2024-06-04 11:51:04 -0300

    Update exercicio_3.py

---
+++ funcoes/exercicio_3.py
@@ -0,0 +1,10 @@
+notas = []
+
+for i in range(4):
+    numero = int(input("Digite suas notas: "))
+    notas.append(notas)
+print("Suas notas:", notas)
+
+soma_notas = sum(notas)
+
+print("Soma dos números:", soma_notas)
