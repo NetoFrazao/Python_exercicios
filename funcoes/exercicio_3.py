af8b3434bd25c8b624cbb8fbf9f87c181965832d
NetoFrazao <joaofrazao054@gmail.com>
2024-06-04 12:03:17 -0300

    Update exercicio_3.py

--- funcoes/exercicio_3.py
+++ funcoes/exercicio_3.py
@@ -1,10 +1,25 @@
+def calcular_media(notas):
+    soma = sum(notas)
+    media = soma / len(notas)
+    return media
+
+def aprovacao(media):
+    if media >= 5:
+        return f"Aprovado com média {media:.2f}"
+    else:
+        return f"Reprovado com média {media:.2f}"
+
+nome_aluno = input("Digite o nome do aluno: ")
+
 notas = []
 
 for i in range(4):
-    numero = int(input("Digite suas notas: "))
-    notas.append(notas)
-print("Suas notas:", notas)
+    nota = float(input(f"Digite a nota {i+1}: "))
+    notas.append(nota)
 
-soma_notas = sum(notas)
 
-print("Soma dos números:", soma_notas)
+media = calcular_media(notas)
+
+resultado = aprovacao(media)
+
+print(f"O aluno {nome_aluno} está {resultado}")
